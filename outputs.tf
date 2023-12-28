1e2f9f85014e8556ea50f2fcc0c6491d98c09711
SergSha <sy.shagalov@yandex.ru>
2023-12-28 19:03:10 +0300

    edit outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,7 +12,7 @@ output "dbs-info" {
 output "bes-info" {
   description = "General information about created VMs"
   value = {
-    for vm in proxmox_vm_qemu.dbs[*] :
+    for vm in proxmox_vm_qemu.bes[*] :
     vm.name => {
       ip_address = vm.default_ipv4_address
     }
@@ -22,7 +22,9 @@ output "bes-info" {
 output "lbs-info" {
   description = "General information about created VMs"
   value = {
-    for vm in proxmox_vm_qemu.dbs[*] :
-    vm.name => vm
+    for vm in proxmox_vm_qemu.lbs[*] :
+    vm.name => {
+      ip_address = vm.default_ipv4_address
+    }
   }
 }
